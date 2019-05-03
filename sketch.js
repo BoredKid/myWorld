167833eb00acdfb0d34244376fdff4c7624b5c85
BoredKid <cisskane@gmail.com>
2019-05-03 02:34:35 +0200

    add asgal wiki ball

--- sketch.js
+++ sketch.js
@@ -11,6 +11,7 @@ function setup() {
     balls.tab.push(new Ball(width / 2, height / 2, 40, color("purple"), "http://drawingbot.boredkid.io", "DrawingBot"));
     balls.tab.push(new Ball(width, height, 40, color("blue"), "https://www.linkedin.com/in/cisskane/", "LinkedIn"));
     balls.tab.push(new Ball(width / 5, height, 40, color("#333"), "https://github.com/BoredKid", "Github "));
+    balls.tab.push(new Ball( 4 * width / 5, 4 * height / 7, 40, color("#5C4236"), "https://asgal.fandom.com/fr/wiki/Asgal", "  Asgal  "));
 }
 
 function draw() {
